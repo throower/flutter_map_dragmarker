4d5b161b7c2a3ee723e13a5083f3f7109a9bf9b8
santiago.nunez <santiago@mitulagroup.com>
2021-09-10 19:09:50 +0200

    Update to latlong2

--- lib/dragmarker.dart
+++ lib/dragmarker.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
 import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
 
 
 class DragMarkerPluginOptions extends LayerOptions {
